c8c1596a7dacbf989434df9c8c87927227344fc5
dushyantbehl <dushyantbehl@hotmail.com>
2014-03-05 05:58:01 +0530

    Small changes here and there.

--- openMPI/mpi-average.cpp
+++ openMPI/mpi-average.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include <mpi.h>
 
 #define N_ARGS 14 // Number of arguments required by the program.
@@ -13,6 +14,7 @@
 #define SUCCESS 0
 #define DELM " "
 
+/* TODO: If needed add some error handling while reading input.*/
 void readMatrix(FILE* infile, float *A, int m, int n){
   char *line = NULL;
   size_t len = 0;
@@ -30,17 +32,17 @@ void readMatrix(FILE* infile, float *A, int m, int n){
     i++;
   }
 
-  printf("Read Matrix A:- \n");
+  printf("\nRead Matrix A:- \n");
   for (i=0; i<m; i++) {
     printf("\t| ");
     for (j=0; j<n; j++)
       printf("%4.4f ", A[i*n+j]);
     printf("|\n");
   }
+  printf("\n");
 }
 
 void outputMatrix(FILE* outfile, float *A, int m, int n){
-  
   int i,j;
   
   for(i=0; i<m; i++){
@@ -52,13 +54,14 @@ void outputMatrix(FILE* outfile, float *A, int m, int n){
     fprintf(outfile,"\n");
   }
   
-  printf("Output Matrix A:- \n");
+  printf("\nOutput Matrix A:- \n");
   for (i=0; i<m; i++) {
     printf("\t| ");
     for (j=0; j<n; j++)
       printf("%4.4f ", A[i*n+j]);
     printf("|\n");
   }
+  printf("\n");
 }
 
 int main( int argc, char *argv[]){
@@ -79,6 +82,7 @@ int main( int argc, char *argv[]){
     if(argc < N_ARGS){
       printf("Please specify proper arguments - ");
       printf("usage:- %s infile outfile m n p q s l a b c d e\n",argv[0]);
+      MPI_Finalize();
       exit(FAILURE);
     }
     /*Open files for input output.*/
@@ -86,6 +90,7 @@ int main( int argc, char *argv[]){
     outfile = fopen(argv[2],"w");
     if( infile == NULL || outfile == NULL){
       printf("Problem opening input output files\n");
+      MPI_Finalize();
       exit(FAILURE);
     }
   }
@@ -102,16 +107,32 @@ int main( int argc, char *argv[]){
   d = atof(argv[12]);
   e = atof(argv[13]);
   
+  /*We don't want our program to continue with wrong input.*/
+  assert( m>0 && n>0 && p>0 && q>0 && s>0 && l>0 );
+  
+  /*Leader gets complete matrix A and will transfer blocks to others.*/
   if(myrank == 0){
     A = (float *)malloc(m*n*(sizeof(float)));
     readMatrix(infile,A,m,n);
   }
-    
+  
+  /*Perform l iterations of averaging over the matrix.*/
+  while( (l--)>0 ){
+    if(myrank == 0)
+      printf("Iteration number %d\n",l+1);
+  }
+  
   if(myrank == 0){
     outputMatrix(outfile,A,m,n);
-    if(fcloseall() != 0)
+    printf("Output written to the file %s\n",argv[2]);
+    /*Close all the files before exiting.*/
+    if(fcloseall() != 0){
       printf("Error closing the files\n");
+      MPI_Finalize();
+      exit(FAILURE);
+    }
   }
+  
   MPI_Finalize();
-  return FAILURE;
+  return SUCCESS;
 }
