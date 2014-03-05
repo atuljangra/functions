5762dfa02abb13d4b1741fc295e1b806f7a02146
dushyantbehl <dushyantbehl@hotmail.com>
2014-03-05 22:54:36 +0530

    Moving your changes to the end of the file for now.

--- openMPI/mpi-average.cpp
+++ openMPI/mpi-average.cpp
@@ -282,7 +282,7 @@ int main( int argc, char *argv[]){
            //~ 
 //~ 
         //~ }
-//~ =======
+//~
 //~ 
   //~ /*                 
    //~ * We need to gather data to master from all the worker threads.
